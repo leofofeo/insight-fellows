19c23c3bca1399ab10d5e425692466f07fd6d203
Leo Rubiano <leofofeo@Leos-MacBook-Pro.local>
2020-04-16 21:06:34 -0500

    outline data structures and model of data conversion to be used for csv output

--- src/reading/handle_csv.py
+++ src/reading/handle_csv.py
@@ -2,17 +2,21 @@ import csv
 
 # want to return:
 # - product "all lower case"
+# -- Product class
+# --- no. of complaints (below) counter property
+# --- no. of companies receiving complaint for that product and year (set)
 # - year
 # - total number of complaints for that product in that year
-# - total number of companies receiving at least one complaint for that product and year
 # - highest percentage (rounded to the nearest whoel number) of total complaints filed against one company for that product and year
+# -- calculated property
 
 """
 Model dict for data we want to return:
 
 {
     "2019": {
-
+        dict of product class where the key is a shorthand for the prodcut name, and the class contains the information listed above
+        and total number of complaints for each class is a property on that object
     },
     "2020": {
 
